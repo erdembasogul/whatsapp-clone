f8eafbcdc55547a9632402d34c5456511cdb43cd
ecusee <erdobasogul@gmail.com>
2021-01-23 15:51:27 +0300

    chatListItemComponent

---
+++ components/ChatListItem/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import { ChatRoom } from '../../types';
+import styles from './style';
+
+export type ChatListItemProps = {
+    chatRoom: ChatRoom;
+}
+
+const ChatListItem = (props: ChatListItemProps) => {
+    const { chatRoom } = props;
+
+    const user = chatRoom.users[1]
+    
+    return (
+        <View style={styles.container}>
+            <View style={styles.leftContainer}>
+                <Image source={{uri: user.imageUri}} style={styles.avatar} />
+                <View style={styles.midContainer}>
+                    <Text style={styles.username}>{user.name}</Text>
+                    <Text style={styles.lastMessage}>{chatRoom.lastMessage.content}</Text>
+                </View>
+            </View>
+            {/* <Text>{chatRoom.lastMessage.createdAt}</Text> */}
+            <Text style={styles.time}>Yesterday</Text>
+        </View>
+    )
+}
+
+export default ChatListItem;
---
+++ data/ChatRooms.ts
@@ -0,0 +1,145 @@
+export default [{
+    id: '1',
+    users: [{
+      id: 'u1',
+      name: 'Erdem',
+      imageUri: 'https://randomuser.me/api/portraits/men/80.jpg',
+    }, {
+      id: 'u2',
+      name: 'Fatih',
+      imageUri: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb',
+    }],
+    lastMessage: {
+      id: 'm1',
+      content: 'Well done this sprint, guys!',
+      createdAt: '2020-10-03T14:48:00.000Z',
+    }
+  }, {
+    id: '2',
+    users: [{
+      id: 'u1',
+      name: 'Erdem',
+      imageUri: 'https://randomuser.me/api/portraits/men/80.jpg',
+    }, {
+      id: 'u3',
+      name: 'Enes',
+      imageUri: 'https://uifaces.co/our-content/donated/IPh6PTBx.jpg',
+    }],
+    lastMessage: {
+      id: 'm2',
+      content: 'How are you doing?',
+      createdAt: '2020-10-02T15:40:00.000Z',
+    }
+  }, {
+    id: '3',
+    users: [{
+      id: 'u1',
+      name: 'Erdem',
+      imageUri: 'https://randomuser.me/api/portraits/men/80.jpg',
+    }, {
+      id: 'u4',
+      name: 'Omer',
+      imageUri: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTAyNTAzMTA4OTJeQTJeQWpwZ15BbWU3MDA4NDI2Njk@._V1_UX172_CR0,0,172,256_AL_.jpg',
+    }],
+    lastMessage: {
+      id: 'm3',
+      content: 'Hi, Erdem.',
+      createdAt: '2020-10-02T14:48:00.000Z',
+    }
+  }, {
+    id: '4',
+    users: [{
+      id: 'u1',
+      name: 'Erdem',
+      imageUri: 'https://randomuser.me/api/portraits/men/80.jpg',
+    }, {
+      id: 'u5',
+      name: 'Yakup',
+      imageUri: 'https://uifaces.co/our-content/donated/VUMBKh1U.jpg',
+    }],
+    lastMessage: {
+      id: 'm4',
+      content: 'Can you review my last merge',
+      createdAt: '2020-09-29T14:48:00.000Z',
+    }
+  }, {
+    id: '5',
+    users: [{
+      id: 'u1',
+      name: 'Erdem',
+      imageUri: 'https://randomuser.me/api/portraits/men/80.jpg',
+    }, {
+      id: 'u6',
+      name: 'Elon Musk',
+      imageUri: 'https://randomuser.me/api/portraits/men/92.jpg',
+    }],
+    lastMessage: {
+      id: 'm5',
+      content: 'I would be happy',
+      createdAt: '2020-09-30T14:48:00.000Z',
+    }
+  }, {
+    id: '6',
+    users: [{
+      id: 'u1',
+      name: 'Erdem',
+      imageUri: 'https://randomuser.me/api/portraits/men/80.jpg',
+    }, {
+      id: 'u7',
+      name: 'Kemal',
+      imageUri: 'https://randomuser.me/api/portraits/men/90.jpg',
+    }],
+    lastMessage: {
+      id: 'm6',
+      content: 'I have a solution',
+      createdAt: '2020-10-02T15:40:00.000Z',
+    }
+  }, {
+    id: '7',
+    users: [{
+      id: 'u1',
+      name: 'Erdem',
+      imageUri: 'https://randomuser.me/api/portraits/men/80.jpg',
+    }, {
+      id: 'u8',
+      name: 'Adem',
+      imageUri: 'https://uifaces.co/our-content/donated/9-mQNf2U.jpg',
+    }],
+    lastMessage: {
+      id: 'm7',
+      content: 'How are you doing?',
+      createdAt: '2020-10-02T15:40:00.000Z',
+    }
+  }, {
+    id: '8',
+    users: [{
+      id: 'u1',
+      name: 'Erdem',
+      imageUri: 'https://randomuser.me/api/portraits/men/80.jpg',
+    }, {
+      id: 'u9',
+      name: 'Mom',
+      imageUri: 'https://images.unsplash.com/photo-1541710430735-5fca14c95b00?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+    }],
+    lastMessage: {
+      id: 'm8',
+      content: 'Dear, did you eat?',
+      createdAt: '2020-09-27T15:40:00.000Z',
+    }
+  }, {
+    id: '9',
+    users: [{
+      id: 'u1',
+      name: 'Erdem',
+      imageUri: 'https://randomuser.me/api/portraits/men/80.jpg',
+    }, {
+      id: 'u10',
+      name: 'Angelina Jolie',
+      imageUri: 'https://images.unsplash.com/photo-1476956207128-beccf17c1771?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=52bbdebca6084fffac43984574db3038',
+    }],
+    lastMessage: {
+      id: 'm9',
+      content: 'Meet me at the same place',
+      createdAt: '2020-09-25T15:40:00.000Z',
+    },
+  }]
